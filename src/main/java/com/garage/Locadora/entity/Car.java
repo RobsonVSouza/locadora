e3ec7fb2fb2e7fff5415cb2ed28b02af98f21ebb
Robson Souza <robson.ppmg@gmail.com>
2025-03-03 08:35:57 -0300

    Add new properties

--- src/main/java/com/garage/Locadora/entity/Car.java
+++ src/main/java/com/garage/Locadora/entity/Car.java
@@ -33,7 +33,7 @@ public class Car implements Serializable {
     private BigDecimal dailyRate;
 
     @Min(1)
-    @Max(7)
+    @Max(5)
     private int avaliable = 1;
 
     private String licensePlate;
